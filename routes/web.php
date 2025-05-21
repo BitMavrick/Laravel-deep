0b5cbc1b2a28b2fe72677e0a9f9f0eef75ac9e9b
mehedi <hello.mehedi@outlook.com>
2025-05-21 14:37:03 +0600

    Setting up the route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CurrencyController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/convert', [CurrencyController::class, 'convert']);
